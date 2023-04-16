0ca1f0964e0c26d0e19e780d7b298c4ceb652209
Farhan Hilmi <farhanhilmi077@gmail.com>
2023-04-16 22:32:23 +0700

    add response format

--- src/api/users.js
+++ src/api/users.js
@@ -1,3 +1,5 @@
+import { responseData } from '../utils/responses.js';
+
 // import userServices from '../services/index.js';
 export class UsersController {
     constructor(channel) {
@@ -8,11 +10,7 @@ export class UsersController {
         try {
             // const { userId, roles } = JSON.parse(req.header('user'));
             // const data = await this.authService.getUserData({ userId, roles });
-            // res.status(200).json({
-            //     status: 'OK',
-            //     message: 'success fetching data',
-            //     data,
-            // });
+            res.status(200).json(responseData({}));
         } catch (error) {
             next(error);
         }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import { UsersController } from '../api/users.js';
 
 const Routes = (channel) => {
     const router = Router();
+    const controller = new UsersController(channel);
+    router.get('/', controller.getUser.bind(controller));
 
     return router;
 };
